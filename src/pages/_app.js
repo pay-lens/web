fa6c58a8a6d21fce8da10572813ca1ac74aa7d5c
Holly Springsteen <hhspringsteen@gmail.com>
2021-02-25 13:12:12 -0600

    get styles working in app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+// CSS Reset
+import '../styles/utils/reset.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+// Normalize CSS (https://necolas.github.io/normalize.css/)
+import '../styles/utils/normalize.css';
+
+// General Styles
+import '../styles/base.css';
+
+// Helper Files
+import '../styles/helpers/typography.css';
+import '../styles/helpers/utils.css';
+import '../styles/helpers/sr-only.css';
+
+const MyApp = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
+);
 
 export default MyApp
